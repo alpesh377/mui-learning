f2a4b3591936c2a3c543d24ca1fd7029d853c5db
alpesh377 <alpeshb@zignuts.com>
2025-02-28 15:07:27 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,154 @@
+'use client'
+
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import { z } from "zod";
+import { useEffect, useState } from "react";
+import { schema } from "./schema";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { DefaultValues } from "react-hook-form";
+
+export default function Home() {
+  type formData = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors, touchedFields },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const password = watch("password")
+  
+  const defaultValues:DefaultValues<formData>={
+    email: "alpeshbaria@gmail.com",
+    password: "",
+    phoneNumber: "",
+    country: "",
+    gender:null
+      }
+
+  // useEffect(()=>{
+  //   console.log(password);
+  //   if (password.length < 8 ) {
+  //     console.log("Password is too short")
+  //   } else {
+  //     console.log("password is 8 letters long")
+      
+  //   } 
+  // },[password])
+  const onSubmit = (data: formData) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        align="left"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography variant="h4" gutterBottom>
+          Signup
+        </Typography>
+
+        <TextField
+          label="Email"
+          fullWidth
+          margin="normal"
+          defaultValue={defaultValues.email}
+          error={!!errors.email && touchedFields.email}
+          {...register("email")}
+          helperText={errors.email?.message}
+        />
+
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          defaultValue={defaultValues.password}
+          {...register("password")}
+          error={!!errors.password && touchedFields.password}
+          helperText={errors.password?.message}
+        />
+        <TextField
+          label="Phone Number"
+          fullWidth
+          margin="normal"
+          {...register("phoneNumber")}
+          error={!!errors.phoneNumber && touchedFields.phoneNumber}
+          helperText={errors.phoneNumber?.message}
+        />
+
+        <FormControl fullWidth margin="normal" error={!!errors.country && touchedFields.country}>
+          <InputLabel>Country</InputLabel>
+          <Select
+            label="Country"
+            {...register("country")}
+            defaultValue={defaultValues.country}
+            sx={{ my: 1 }}
+          >
+            <MenuItem value="USA">USA</MenuItem>
+            <MenuItem value="Canada">Canada</MenuItem>
+            {/* Add other countries as needed */}
+          </Select>
+          <FormHelperText>{errors.country?.message}</FormHelperText>
+        </FormControl>
+
+
+        <section>
+            <label>Radio Group</label>
+            <Controller
+              defaultValue={defaultValues.gender}
+              render={({ field }) => (
+                <RadioGroup aria-label="gender" {...field}>
+                  <FormControlLabel
+                    value="female"
+                    // defaultValue={defaultValues.option}
+                    control={<Radio />}
+                    label="Female"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Male"
+                  />
+                </RadioGroup>
+              )}
+              name="gender"
+              control={control}
+            />
+          </section>
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+}
---
+++ app/schema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+export const schema = z.object({
+    email: z.string().email("Email is Invalid").min(1,"Email is required"),
+    password: z.string().min(6,"Password must be 6 letters long"),
+    phoneNumber: z.string().min(10,"number must 10 digit long").max(10,"enter 10 digit number"),
+    country : z.string().min(1,"Country is required"),
+    gender: z.union([z.enum(["yes", "no"]), z.null()]).nullable()
+      
+})
